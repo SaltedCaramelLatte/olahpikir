852c5c25a7625ddf1f18b9c6b2c9f5546aeae052
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-10-31 21:16:41 +0800

    add Home component with HeroSection

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// src/pages/Home.tsx
+import HeroSection from '@/components/home/HeroSections';
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
