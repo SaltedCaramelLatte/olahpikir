2e8b6a2433b8cfbf7e9ba4b0776d5ef95dd31a89
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-01 19:18:33 +0800

    refactor Tailwind CSS configuration for improved color palette and theme support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 // import { nextui } from '@nextui-org/react'
-import defaultTheme from 'tailwindcss/defaultTheme'
-import typography from '@tailwindcss/typography'
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|tabs|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|tabs|ripple|spinner).js",
   ],
 
   theme: {
@@ -18,20 +18,20 @@ export default {
       },
       colors: {
         light: {
-          primary: '#FFD73B',
-          secondary: '#1a1a2e',
-          background: '#ffffff',
-          text: '#333333',
-          accent: '#FF6B6B',
-          border: '#E0E0E0',
+          primary: '#A65E2E', // Cokelat biji kopi untuk warna utama
+          secondary: '#DDB892', // Warna krem atau beige untuk elemen sekunder
+          background: '#F3EDE2', // Warna latar belakang terang, krem
+          text: '#5D4037', // Cokelat gelap untuk teks pada latar terang
+          accent: '#C59B6D', // Warna aksen yang lebih lembut
+          border: '#E0D3C1', // Warna border yang lembut
         },
         dark: {
-          primary: '#FFD73B',
-          secondary: '#F2F2F2',
-          background: '#1a1a2e',
-          text: '#e0e0e0',
-          accent: '#FF6B6B',
-          border: '#333333',
+          primary: '#DDB892', // Krem sebagai warna utama pada mode gelap
+          secondary: '#A65E2E', // Cokelat sebagai warna sekunder pada mode gelap
+          background: '#3E2723', // Cokelat gelap atau hitam legam sebagai latar belakang gelap
+          text: '#E8D5B7', // Warna krem terang untuk teks pada latar gelap
+          accent: '#C59B6D', // Warna aksen yang lebih lembut
+          border: '#5D4037', // Warna border yang lebih gelap pada mode gelap
         },
       },
     },
