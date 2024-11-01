4a63891cca868eaa082fa1db66b80eba81f9b283
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-01 16:44:50 +0800

    add Navbar component to Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/home/HeroSections';
 import Footer from '@/components/Footer'; // Pastikan path ini benar
+import Navbar from '@/components/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar /> {/* Memanggil Navbar di atas HeroSection */}
       <HeroSection />
       <Footer /> {/* Memanggil Footer di bawah HeroSection */}
     </div>
