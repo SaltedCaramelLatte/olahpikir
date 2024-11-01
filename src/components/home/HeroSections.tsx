49d0dd1d6378ce0d4a10bd0dd9176bfda82b2e86
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-02 00:45:33 +0800

    refactor: update HeroSections and MenuSection components; enhance menu item rendering and styling

--- src/components/home/HeroSections.tsx
+++ src/components/home/HeroSections.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <section className="relative flex flex-col md:flex-row items-center justify-center min-h-screen bg-light-background dark:bg-dark-background py-10 px-4 sm:px-6 lg:px-8">
             {/* Konten Teks */}
             <div className="flex flex-col items-start md:w-1/2 max-w-lg text-left md:ml-10">
-                <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold font-bossa text-primary mb-4 leading-tight">
+                <h1 className="font-bold font-bossa text-6xl sm:text-5xl lg:text-6xl text-primary mb-4 leading-tight">
                     olahpikir Cafe
                 </h1>
                 <p className="text-xl sm:text-2xl text-dark-secondary dark:text-light-secondary mb-6">
@@ -27,7 +27,7 @@ const HeroSection = () => {
             {/* Gambar Produk */}
             <div className="relative z-10 md:w-1/2 flex justify-center mt-8 md:mt-0 md:mr-10">
                 <Image
-                     src={coffeeImage} // Pastikan path ini benar
+                    src={coffeeImage} // Pastikan path ini benar
                     alt="Gelas kopi"
                     className="object-contain"
                     width={500}
--- src/components/home/MenuSection.tsx
+++ src/components/home/MenuSection.tsx
@@ -2,19 +2,31 @@ import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { useEffect, useRef, useState } from "react";
 
 const MenuSection = () => {
-    const list = [
+    const coffeeList = [
+        { title: "Espresso", img: "/images/coffee-1.jpeg", price: "$4.00" },
+        { title: "Latte", img: "/images/coffee-2.jpeg", price: "$5.00" },
+        { title: "Cappuccino", img: "/images/coffee-3.jpeg", price: "$5.50" },
+        { title: "Espresso", img: "/images/coffee-1.jpeg", price: "$4.00" },
+        { title: "Latte", img: "/images/coffee-2.jpeg", price: "$5.00" },
+        { title: "Cappuccino", img: "/images/coffee-3.jpeg", price: "$5.50" },
+        { title: "Espresso", img: "/images/coffee-1.jpeg", price: "$4.00" },
+        { title: "Latte", img: "/images/coffee-2.jpeg", price: "$5.00" },
+        { title: "Cappuccino", img: "/images/coffee-3.jpeg", price: "$5.50" },
+    ];
+
+    const nonCoffeeList = [
         { title: "Orange", img: "/images/fruit-1.jpeg", price: "$5.50" },
         { title: "Tangerine", img: "/images/fruit-2.jpeg", price: "$3.00" },
         { title: "Raspberry", img: "/images/fruit-3.jpeg", price: "$10.00" },
-        { title: "Lemon", img: "/images/fruit-4.jpeg", price: "$5.30" },
-        { title: "Avocado", img: "/images/fruit-5.jpeg", price: "$15.70" },
-        { title: "Lemon 2", img: "/images/fruit-6.jpeg", price: "$8.00" },
-        { title: "Banana", img: "/images/fruit-7.jpeg", price: "$7.50" },
-        { title: "Watermelon", img: "/images/fruit-8.jpeg", price: "$12.20" },
+        { title: "Orange", img: "/images/fruit-1.jpeg", price: "$5.50" },
+        { title: "Tangerine", img: "/images/fruit-2.jpeg", price: "$3.00" },
+        { title: "Raspberry", img: "/images/fruit-3.jpeg", price: "$10.00" },
+        { title: "Orange", img: "/images/fruit-1.jpeg", price: "$5.50" },
+        { title: "Tangerine", img: "/images/fruit-2.jpeg", price: "$3.00" },
+        { title: "Raspberry", img: "/images/fruit-3.jpeg", price: "$10.00" },
     ];
 
-    const [visibleItems, setVisibleItems] = useState(Array(list.length).fill(false));
-
+    const [visibleItems, setVisibleItems] = useState(Array(coffeeList.length + nonCoffeeList.length).fill(false));
     const observer = useRef<IntersectionObserver | null>(null);
 
     useEffect(() => {
@@ -35,63 +47,72 @@ const MenuSection = () => {
         });
 
         const elements = document.querySelectorAll('.menu-item');
-        elements.forEach((item) => {
-            observer.current?.observe(item);
-        });
+        elements.forEach((item) => observer.current?.observe(item));
 
         return () => {
-            if (observer.current) {
-                elements.forEach((item) => {
-                    observer.current?.unobserve(item);
-                });
-            }
+            elements.forEach((item) => observer.current?.unobserve(item));
         };
     }, []);
 
-    return (
-        <section className="relative flex flex-col items-center justify-center min-h-screen bg-light-background dark:bg-dark-background py-10 px-4 sm:px-6 lg:px-8">
-            <h2 className="text-4xl font-bold text-primary mb-8">Our Menu</h2>
-            <div className="gap-6 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-                {list.map((item, index) => {
-                    const [imageError, setImageError] = useState(false);
+    const renderMenuItems = (list: { title: string; img: string; price: string; }[], offset: number) => {
+        return list.map((item: { title: string; img: string; price: string; }, index: number) => {
+            const [imageError, setImageError] = useState(false);
+            return (
+                <div
+                    className={`menu-item transition-opacity duration-1000 ${visibleItems[index + offset] ? 'opacity-100' : 'opacity-0'}`}
+                    data-index={index + offset}
+                    key={index + offset}
+                >
+                    <Card
+                        shadow="lg"
+                        isPressable
+                        radius="lg"
+                        className="rounded-xl hover:scale-105 transition-transform bg-gray-50 dark:bg-gray-800 overflow-hidden w-[200px] h-[400px]"
+                        onPress={() => console.log(`${item.title} pressed`)}
+                    >
 
-                    return (
-                        <div
-                            className={`menu-item transition-opacity duration-1000 ${visibleItems[index] ? 'opacity-100' : 'opacity-0'}`}
-                            data-index={index}
-                            key={index}
-                        >
-                            <Card
-                                shadow="lg"
-                                isPressable
-                                className="rounded-xl hover:scale-105 transition-transform bg-gray-50 dark:bg-gray-900 overflow-hidden"
-                                onPress={() => console.log(`${item.title} pressed`)}
-                            >
-                                <CardBody className="overflow-hidden p-0">
-                                    {imageError ? (
-                                        <div className="bg-gray-200 dark:bg-gray-700 w-full h-[180px] flex items-center justify-center">
-                                            <span className="text-gray-500 dark:text-gray-300">Image Placeholder</span>
-                                        </div>
-                                    ) : (
-                                        <Image
-                                            shadow="sm"
-                                            alt={item.title}
-                                            className="w-full h-[180px] object-cover"
-                                            src={item.img}
-                                            onError={() => setImageError(true)}
-                                        />
-                                    )}
-                                </CardBody>
-                                <CardFooter className="flex justify-between items-center p-3 bg-gray-100 dark:bg-gray-800">
-                                    <b className="text-sm text-primary">{item.title}</b>
-                                    <p className="text-sm text-gray-600 dark:text-gray-400">{item.price}</p>
-                                </CardFooter>
-                            </Card>
-                        </div>
-                    );
-                })}
+                        <CardBody className="overflow-hidden p-0">
+                            {imageError ? (
+                                <div className="bg-gray-200 dark:bg-gray-700 w-full h-[400px] flex items-center justify-center rounded-b-lg">
+                                    <span className="text-gray-500 dark:text-gray-300">Image Placeholder</span>
+                                </div>
+                            ) : (
+                                <Image
+                                    shadow="sm"
+                                    alt={item.title}
+                                    className="w-full h-[180px] object-cover"
+                                    src={item.img}
+                                    onError={() => setImageError(true)}
+                                />
+                            )}
+                        </CardBody>
+
+                        <CardFooter className="flex justify-between items-center p-3 bg-gray-100 dark:bg-gray-800">
+                            <b className="text-sm text-primary">{item.title}</b>
+                            <p className="text-sm text-gray-600 dark:text-gray-400">{item.price}</p>
+                        </CardFooter>
+                    </Card>
+                </div>
+            );
+        });
+    };
+
+    return (
+        <section className="relative flex flex-col items-center justify-center min-h-screen bg-light-background dark:bg-dark-background py-10 px-2 sm:px-4 lg:px-6">
+            <h2 className="text-4xl font-bold text-primary mb-8">Our Menu</h2>
+
+            <h3 className="text-2xl font-semibold text-primary mb-4">Coffee</h3>
+            <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+                {renderMenuItems(coffeeList, 0)}
+            </div>
+
+            <h3 className="text-2xl font-semibold text-primary mb-4">Non-Coffee</h3>
+            <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                {renderMenuItems(nonCoffeeList, coffeeList.length)}
             </div>
         </section>
+
+
     );
 };
 
