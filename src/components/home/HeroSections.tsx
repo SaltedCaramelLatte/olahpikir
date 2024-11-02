a0b37681086aa8c6012f5926eb02ff765c4b3638
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-02 18:52:34 +0800

    style: update HeroSection component; modify WhatsApp message for improved clarity and change button text for better engagement

--- src/components/home/HeroSections.tsx
+++ src/components/home/HeroSections.tsx
@@ -28,10 +28,11 @@ const HeroSection = () => {
                         color="secondary"
                         size="lg"
                         className="bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-md shadow-md hover:bg-[#924F29] dark:hover:bg-[#CCAA84] transition mb-4"
-                        onClick={() => window.open('https://wa.me/6285156967944?text=Halo%20kak,%0A%0Asaya%20dari%20[Nama%20Anda]%20berminat%20mengadakan%20bazaar%20pada%20tanggal%20[Masukkan%20tanggal%20dan%20bulan].%0A%0AApakah%20bisa%20dibantu%3F', '_blank')}
+                        onClick={() => window.open('https://wa.me/6285156967944?text=Halo%20kak,%0A%0Asaya%20dari%20[Nama%20Anda]%20%0Aberminat%20mengadakan%20bazaar%20pada%20tanggal%20[Masukkan%20tanggal%20dan%20bulan].%0A%0AApakah%20bisa%20dibantu%3F', '_blank')}
                     >
-                        Ingin mengadakan Bazaar?
+                        Organisasimu Ingin mengadakan Bazaar?
                     </Button>
+
 
                 </div>
 
