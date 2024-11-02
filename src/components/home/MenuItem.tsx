992c096d36ddc51d02f723066e10932ce1d2287f
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-02 16:29:27 +0800

    style: update MenuItem component; adjust Card dimensions and improve image handling for better layout

--- src/components/home/MenuItem.tsx
+++ src/components/home/MenuItem.tsx
@@ -16,31 +16,32 @@ const MenuItem = ({ item, index, visible }: MenuItemProps) => {
 
     return (
         <div
-            className={`menu-item transition-opacity duration-1000 ${
-                visible ? 'opacity-100' : 'opacity-0'
-            }`}
-            data-index={index}
+        className={`menu-item transition-opacity duration-1000 ${
+            visible ? 'opacity-100' : 'opacity-0'
+        }`}
+        data-index={index}
         >
             <Card
                 shadow="lg"
                 isPressable
                 radius="lg"
-                className="rounded-xl hover:scale-105 transition-transform bg-gray-50 dark:bg-gray-800 overflow-hidden w-full max-w-[350px] lg:max-w-[400px] mx-auto"
+                className="rounded-xl hover:scale-105 transition-transform bg-gray-50 dark:bg-gray-800 overflow-hidden w-[150px] h-[300px]" // Tetapkan ukuran Card di sini
                 onPress={() => console.log(`${item.title} pressed`)}
             >
-                <CardBody className="p-0 h-[400px]"> {/* Mengatur tinggi CardBody agar mengikuti rasio 1:2 */}
+                <CardBody className="p-0 h-[180px]"> {/* Tetapkan tinggi tetap untuk CardBody */}
                     {imageError ? (
-                        <div className="bg-gray-200 dark:bg-gray-700 w-full h-full flex items-center justify-center">
+                        <div className="bg-gray-200 dark:bg-gray-700 w-full h-full flex items-center justify-center rounded-b-lg">
                             <span className="text-gray-500 dark:text-gray-300">Image Placeholder</span>
                         </div>
                     ) : (
                         <Image
-                            style={{ opacity: visible ? 1 : 0.3 }} // Atur opacity di elemen gambar
-                            shadow="sm"
+                        style={{ opacity: visible ? 1 : 0.3 }} // Atur opacity di elemen gambar
+
                             alt={item.title}
-                            className="object-cover w-full h-full" // Memastikan gambar memenuhi CardBody dengan rasio 1:2
                             src={item.img}
                             onError={() => setImageError(true)}
+                            className="w-full h-full object-cover" // Gambar memenuhi area CardBody
+                            // style={{ objectFit: "cover" }} // Pastikan gambar menutupi seluruh CardBody
                         />
                     )}
                 </CardBody>
