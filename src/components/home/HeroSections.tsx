256864ff0f368ac87ae57bf682f74f83411027ec
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-02 19:50:18 +0800

    style: update HeroSection component; adjust button sizes and layout for improved responsiveness

--- src/components/home/HeroSections.tsx
+++ src/components/home/HeroSections.tsx
@@ -14,28 +14,23 @@ const HeroSection = () => {
                 <p className="text-xl sm:text-2xl text-dark-secondary dark:text-light-secondary mb-6">
                     Nikmati berbagai varian kopi yang kami sajikan dengan penuh cinta dan dedikasi.
                 </p>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex flex-col md:flex-row justify-center items-center md:gap-2 gap-4">
                     <Button
                         color="secondary"
-                        size="lg"
+                        size="md"
                         className="bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-md shadow-md hover:bg-[#924F29] dark:hover:bg-[#CCAA84] transition mb-4"
                     >
                         Lihat Menu
                     </Button>
-
-                    {/* Tombol WhatsApp */}
                     <Button
                         color="secondary"
-                        size="lg"
+                        size="md"
                         className="bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-md shadow-md hover:bg-[#924F29] dark:hover:bg-[#CCAA84] transition mb-4"
                         onClick={() => window.open('https://wa.me/6285156967944?text=Halo%20kak,%0A%0Asaya%20dari%20[Nama%20Anda]%20%0Aberminat%20mengadakan%20bazaar%20pada%20tanggal%20[Masukkan%20tanggal%20dan%20bulan].%0A%0AApakah%20bisa%20dibantu%3F', '_blank')}
-                    >Ingin mengadakan Bazaar?
+                    >
+                        Ingin mengadakan Bazaar?
                     </Button>
-
-
                 </div>
-
-
             </div>
 
             <div className="relative z-10 md:w-1/2 flex justify-center mt-8 md:mt-0 md:mr-10 overflow-hidden rounded-xl shadow-lg">
