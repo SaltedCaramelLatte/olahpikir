e224436fdb90044ee20d72b6c368561a6d42c9e6
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-04 18:36:28 +0800

    refactor: remove unused scroll function and clean up HeroSection layout

--- src/components/home/HeroSections.tsx
+++ src/components/home/HeroSections.tsx
@@ -5,20 +5,8 @@ import coffeeImage from "@/images/coffee.jpg";
 const HeroSection = () => {
     const [imageError, setImageError] = useState(false);
 
-    // Fungsi scroll ke section tertentu
-    interface ScrollToSectionProps {
-        id: string;
-    }
-
-    const scrollToSection = (id: ScrollToSectionProps['id']): void => {
-        const section = document.getElementById(id);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
     return (
-        <section className="relative flex flex-col md:flex-row items-center justify-center min-h-screen bg-light-background dark:bg-dark-background pt-24 px-4 lg:px-20 px-4 sm:px-6 lg:px-8">
+        <section className="relative flex flex-col md:flex-row items-center justify-center min-h-screen bg-light-background dark:bg-dark-background px-4 lg:px-20 px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col items-start md:w-1/2 max-w-lg text-left md:ml-10">
                 <h1 className="font-bold font-bossa text-6xl sm:text-5xl lg:text-6xl text-primary mb-4 leading-tight dark:text-gray-200">
                     olahpikir Cafe
@@ -31,7 +19,6 @@ const HeroSection = () => {
                         color="secondary"
                         size="md"
                         className="bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-md shadow-md hover:bg-[#924F29] dark:hover:bg-[#CCAA84] transition mb-4"
-                        onClick={() => scrollToSection('menu')} // Fungsi scroll
                     >
                         Lihat Menu
                     </Button>
@@ -42,10 +29,13 @@ const HeroSection = () => {
                         size="md"
                         className="bg-light-primary text-light-background dark:bg-dark-primary dark:text-dark-background rounded-md shadow-md hover:bg-[#924F29] dark:hover:bg-[#CCAA84] transition mb-4"
                         onClick={() => window.open('https://wa.me/6285156967944?text=Halo%20kak,%0A%0Asaya%20dari%20[Nama%20Anda]%20%0Aberminat%20mengadakan%20bazaar%20pada%20tanggal%20[Masukkan%20tanggal%20dan%20bulan].%0A%0AApakah%20bisa%20dibantu%3F', '_blank')}
-                    >
-                        Ingin mengadakan Bazaar?
+                    >Ingin mengadakan Bazaar?
                     </Button>
+
+
                 </div>
+
+
             </div>
 
             <div className="relative z-10 md:w-1/2 flex justify-center mt-8 md:mt-0 md:mr-10 overflow-hidden rounded-xl shadow-lg">
