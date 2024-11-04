37769c5ce1f217debef2b03d861aa4316b3ad441
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-04 15:43:15 +0800

    feat: replace standard button with NextUI Button component in MenuListPage

--- src/components/home/menuList/MenuListPage.tsx
+++ src/components/home/menuList/MenuListPage.tsx
@@ -2,6 +2,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import MenuItem from "../MenuItem";
 import { coffeeList, nonCoffeeList, milkList, MenuItemType } from "./menuData";
+import { Button } from "@nextui-org/react";
 
 const MenuListPage = () => {
     const { category } = useParams<{ category: string }>();
@@ -15,12 +16,14 @@ const MenuListPage = () => {
     return (
         <section className="flex flex-col items-center justify-center min-h-screen bg-light-background dark:bg-dark-background py-10 px-4 lg:px-20">
             {/* Tombol Kembali */}
-            <button
+            <Button
                 onClick={() => navigate(-1)}
-                className="self-start mb-6 px-4 py-2 bg-primary text-white rounded-full hover:bg-secondary transition duration-200"
+                color="primary"
+                variant="faded"
+                className="self-start mb-6 text-light-text bg-light-secondary dark:text-dark-text dark:bg-dark-secondary rounded-full shadow-md hover:bg-light-accent dark:hover:bg-dark-accent transition duration-200"
             >
                 ← Kembali
-            </button>
+            </Button>
 
             <h3 className="text-4xl font-bold text-primary dark:text-gray-200 mb-8 capitalize">
                 {category} Menu
