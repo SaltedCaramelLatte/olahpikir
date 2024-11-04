74f25adb4e22d4607ab39d2561bfbfbb373c5e66
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-04 15:20:56 +0800

    feat: implement layout with Navbar and Footer components for consistent page structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MenuListPage from './components/home/menuList/MenuListPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />  {/* Navbar akan muncul di setiap halaman */}
+      <main className="min-h-screen bg-light-background dark:bg-dark-background py-10 px-4 lg:px-20">
+        <Outlet />  {/* Tempat komponen halaman yang dinamis akan dirender */}
+      </main>
+      <Footer />  {/* Footer juga akan muncul di setiap halaman */}
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu/:category" element={<MenuListPage />} /> {/* Rute dinamis */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="menu/:category" element={<MenuListPage />} /> {/* Rute dinamis */}
+        </Route>
       </Routes>
     </Router>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,16 @@
+// src/pages/Home.tsx
 import HeroSection from '@/components/home/HeroSections';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
 import MenuSection from '@/components/home/MenuSection';
 import GallerySection from '@/components/home/GallerySection';
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <div className="relative flex flex-col md:flex-row items-center justify-center min-h-screen bg-light-background dark:bg-dark-background py-10 px-4 sm:px-6 lg:px-8">
         <HeroSection />
       </div>
       <MenuSection />
       <GallerySection />
-      <Footer />
     </div>
   );
 };
